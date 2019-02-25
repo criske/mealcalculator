2da7399cfce1b9b84097de4136da3faae8ab78a0
cristianpela <cristypela05@gmail.com>
2019-02-25 17:16:08 +0200

    removed test crash to crashlytics

--- app/src/main/java/com/crskdev/mealcalculator/MainActivity.kt
+++ app/src/main/java/com/crskdev/mealcalculator/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.crskdev.mealcalculator
 
 import android.os.Bundle
-import androidx.core.view.postDelayed
 import androidx.navigation.findNavController
-import com.crashlytics.android.Crashlytics
 import com.crskdev.mealcalculator.ui.common.HasBackPressedAwareness
 import com.crskdev.mealcalculator.ui.common.di.DiActivity
 import com.crskdev.mealcalculator.utils.navHostFragmentChildFragmentManager
@@ -15,9 +13,6 @@ class MainActivity : DiActivity() {
         super.onCreate(savedInstanceState)
         dependencyGraph().inject(this)
         setContentView(R.layout.activity_main)
-        window.decorView.postDelayed(5000) {
-            Crashlytics.getInstance().crash() // Force a crash
-        }
     }
 
     override fun onNavigateUp(): Boolean = findNavController(R.id.navContainer).navigateUp()
